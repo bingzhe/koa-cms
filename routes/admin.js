d4d1279363fb9853bbe1c31c90051d4bf4c261cc
bingzhe <874177815@qq.com>
2018-11-15 11:13:05 +0800

    登陆和验证码

--- routes/admin.js
+++ routes/admin.js
@@ -3,17 +3,23 @@ const router = require('koa-router')();
 const login = require('./admin/login.js');
 const user = require('./admin/user.js');
 
+const url = require('url');
 
 router.use(async (ctx, next) => {
     //模板引擎配置全局变量
     ctx.state.__HOST__ = "http://" + ctx.request.header.host;
 
+    let pathname = url.parse(ctx.request.url).pathname;
     // 判断是否登录
     if (ctx.session.userinfo) {
         await next();
     } else {
         // 没有登录跳转登录页面
-        if (ctx.url === "/admin/login" || ctx.url === "/admin/login/doLogin") {
+        if (
+            pathname === "/admin/login"
+            || pathname === "/admin/login/doLogin"
+            || pathname === "/admin/login/code"
+        ) {
             await next();
         } else {
             ctx.redirect('/admin/login');
--- routes/admin/login.js
+++ routes/admin/login.js
@@ -2,37 +2,82 @@ const router = require('koa-router')();
 const tools = require('../../module/tools.js');
 const DB = require('../../module/db.js');
 
+//验证码
+const svgCaptcha = require('svg-captcha');
+
+
 router.get('/', async (ctx) => {
     await ctx.render("admin/login");
 });
+
 
 router.post('/doLogin', async (ctx) => {
     console.log(ctx.request.body);
 
     let username = ctx.request.body.username;
     let password = ctx.request.body.password;
+    let code = ctx.request.body.code;
 
 
     // 1.验证
     // 2.数据库匹配
     // 3. 成功之后用户信息写入session
 
-    let result = await DB.find('admin', { 'username': username, 'password': tools.md5(password) });
+    if (code.toLocaleLowerCase() === ctx.session.code.toLocaleLowerCase()) {
+        //后台需要验证码是否合法
 
-    if (result.length > 0) {
-        console.log("成功");
-        console.log(result[0]);
 
-        ctx.session.userinfo = result[0];
-        ctx.redirect(ctx.state.__HOST__ + '/admin');
+        let result = await DB.find('admin', { 'username': username, 'password': tools.md5(password) });
 
+        if (result.length > 0) {
+            console.log("成功");
+
+            ctx.session.userinfo = result[0];
+            ctx.redirect(ctx.state.__HOST__ + '/admin');
+
+        } else {
+            console.log("失败");
+            ctx.render('admin/error', {
+                message: '用户名或者密码错误',
+                redirect: ctx.state.__HOST__ + '/admin/login'
+            })
+        }
     } else {
-        console.log("失败");
+        console.log('验证码失败');
+
+        ctx.render('admin/error', {
+            message: '验证码错误',
+            redirect: ctx.state.__HOST__ + '/admin/login'
+        });
     }
 
+});
+
+//验证码
+router.get('/code', async (ctx) => {
+    let captcha = svgCaptcha.create({
+        size: 4,
+        fontSize: 50,
+        width: 120,
+        height: 34,
+        background: "#cc9966"
+    });
+
+    //数字验证码
+    // let captcha = svgCaptcha.createMathExpr({
+    //     size: 4,
+    //     fontSize: 50,
+    //     width: 120,
+    //     height: 34,
+    //     background: "#cc9966"
+    // });
 
 
+    ctx.session.code = captcha.text;
+    //设置响应头
+    ctx.response.type = "image/svg+xml";
 
+    ctx.body = captcha.data;
 });
 
 module.exports = router.routes();
