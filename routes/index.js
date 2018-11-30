23045ad69bdf16dc50aea6f68103ef7ff6da3a66
bingzhe <874177815@qq.com>
2018-11-30 15:58:57 +0800

    lunbo

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,31 @@
 const router = require('koa-router')();
+const DB = require('../module/db.js');
 
 router.get('/', async (ctx) => {
-    ctx.body = "前台首页";
+
+    let navResult = await DB.find('nav', {}, {}, { sortJson: { 'sort': 1 } });
+    let focusResult = await DB.find('focus', {}, {}, { sortJson: { 'sort': 1 } });
+
+    ctx.render('default/index', {
+        nav: navResult,
+        focus: focusResult
+    });
+});
+
+router.get('/news', async (ctx) => {
+    ctx.render('default/news');
+});
+router.get('/service', async (ctx) => {
+    ctx.render('default/service');
+});
+router.get('/about', async (ctx) => {
+    ctx.render('default/about');
+});
+router.get('/case', async (ctx) => {
+    ctx.render('default/case');
+});
+router.get('/connect', async (ctx) => {
+    ctx.render('default/connect');
 });
 
 module.exports = router.routes();
