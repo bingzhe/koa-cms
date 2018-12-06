5b692d1c1982117fdc42de964b0ebfa6f4df8e56
bingzhe <874177815@qq.com>
2018-12-06 15:53:52 +0800

    seo title

--- routes/index.js
+++ routes/index.js
@@ -11,9 +11,13 @@ router.use(async (ctx, next) => {
         sortJson: { 'sort': 1 }
     });
 
+    //系统信息
+    let setting = await DB.find('setting', {});
+
     //模板引擎配置全局的变量
     ctx.state.nav = navResult;
     ctx.state.pathname = pathname;
+    ctx.state.setting=setting[0];
 
     await next();
 });
@@ -94,6 +98,31 @@ router.get('/connect', async (ctx) => {
 router.get('/content/:id', async (ctx) => {
     let id = ctx.params.id;
     let content = await DB.find('article', { _id: DB.getObjectId(id) });
+
+    /**
+     * 1.根据文章获取文章的分类信息
+     * 2.根据文章分类，去导航表里面获取当前分类的信息url
+     */
+
+    let cateResult = await DB.find('articlecate', { _id: DB.getObjectId(content[0].pid) });
+
+    let navResult = [];
+    if (cateResult[0].pid != 0) {
+        let parentCateResult = await DB.find('articlecate', { '_id': DB.getObjectId(cateResult[0].pid) });
+        navResult = await DB.find('nav', { $or: [{ 'title': cateResult[0].title }, { 'title': parentCateResult[0].title }] });
+
+    } else {
+        navResult = await DB.find('nav', { 'title': cateResult[0].title });
+    }
+
+    if (navResult.length > 0) {
+        //把url赋值给 pathname
+        ctx.state.pathname = navResult[0]['url'];
+
+    } else {
+        ctx.state.pathname = '/';
+    }
+
 
     ctx.render('default/content', { list: content[0] });
 });
